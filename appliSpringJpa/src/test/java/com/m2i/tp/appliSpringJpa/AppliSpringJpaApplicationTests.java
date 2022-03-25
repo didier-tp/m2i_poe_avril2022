534fbb8828ccdd42acb112ecac86593b502882e4
Didier Defrance <didier@d-defrance.fr>
2022-03-25 13:55:52 +0100

    test

--- appliSpringJpa/src/test/java/com/m2i/tp/appliSpringJpa/AppliSpringJpaApplicationTests.java
+++ appliSpringJpa/src/test/java/com/m2i/tp/appliSpringJpa/AppliSpringJpaApplicationTests.java
@@ -23,13 +23,40 @@ class AppliSpringJpaApplicationTests {
 	
 	@Test 
 	public void testAvecSpring() {
+		//séquence de test idéale:
+		//1. créer une nouvelle chose
+		
 		Employe emp1 = new Employe(null,"prenom1","Nom","0102030405","jean.Bon@xyz.com","login","pwd");
 		daoEmploye.insertNew(emp1);
+		Long idEmp = emp1.getEmpId(); //clef primaire auto incrémentée du nouvel employé ajouté en base
 		
+		//2. afficher tout pour vérifier l'ajout
 		List<Employe> employes = daoEmploye.findAll();
 		for(Employe emp : employes) {
 			System.out.println(emp);
 		}
+		
+		//3. récupérer une entité précise via sa clef primaire et l'afficher
+		Employe emp1Relu = daoEmploye.findById(idEmp);
+		System.out.println("emp1Relu=" + emp1Relu);
+		
+		
+		//4. modifier les valeurs en mémoire puis en base
+		emp1Relu.setPhoneNumber("0504030201");
+		daoEmploye.update(emp1Relu);
+		
+		//5. re-déclencher étape 3 pour vérifier la mise à jour en base
+		Employe emp1Relu2 = daoEmploye.findById(idEmp);
+		System.out.println("emp1Relu2 apres update =" + emp1Relu2);
+		
+		//6. supprimer la chose ajoutée à l'étape 1
+		daoEmploye.deleteById(idEmp);
+		
+		//7. on vérifie que ça n'existe plus
+		Employe emp1Relu3NormalementNull = daoEmploye.findById(idEmp);
+		if(emp1Relu3NormalementNull==null)
+			System.out.println("employe bien supprimé dans la base de données");
+	     
 	}
 
 }
