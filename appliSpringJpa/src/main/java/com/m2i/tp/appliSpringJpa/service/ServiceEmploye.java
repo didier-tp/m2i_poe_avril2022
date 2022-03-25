e0fd8a7db116593fe1c8fe1847053a5b9c2b75f0
Didier Defrance <didier@d-defrance.fr>
2022-03-25 11:36:18 +0100

    service

---
+++ appliSpringJpa/src/main/java/com/m2i/tp/appliSpringJpa/service/ServiceEmploye.java
@@ -0,0 +1,10 @@
+package com.m2i.tp.appliSpringJpa.service;
+
+import java.util.List;
+
+import com.m2i.tp.appliSpringJpa.entity.Employe;
+
+public interface ServiceEmploye {
+   public List<Employe> rechercherTousEmploye();
+   //...
+}
