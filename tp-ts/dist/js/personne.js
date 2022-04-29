e0b9fb7417a6ce73d60cd89b5aea770cd56f348f
Didier Defrance <didier@d-defrance.fr>
2022-04-29 16:18:22 +0200

    get/set

--- tp-ts/dist/js/personne.js
+++ tp-ts/dist/js/personne.js
@@ -2,22 +2,43 @@
 //à faire en Tp : coder le début d'une classe Personne
 class Personne {
     constructor(numero = 0, prenom = "?", nom = "?", age = 0) {
+        this._age = 0;
         this.numero = numero;
         this.prenom = prenom;
         this.nom = nom;
         this.age = age;
     }
+    get age() {
+        return this._age;
+    }
+    set age(newAge) {
+        if (newAge >= 0)
+            this._age = newAge;
+        else
+            throw "age negatif invalide";
+    }
     incrementerAge() {
-        this.age++;
+        this._age++;
     }
 }
-let p1;
-p1 = new Personne(1, "jean", "Bon", 33);
-p1.incrementerAge(); //nouvel age 34
-console.log(`prenom de p1=${p1.prenom}`);
-console.log(JSON.stringify(p1));
+try {
+    let p1;
+    p1 = new Personne(1, "jean", "Bon", -33);
+    p1.incrementerAge(); //nouvel age 34
+    console.log(`prenom de p1=${p1.prenom}`);
+    console.log(JSON.stringify(p1));
+}
+catch (ex) {
+    console.log("exception:" + ex);
+}
 let p2;
 p2 = new Personne();
+try {
+    p2.age = -44;
+}
+catch (ex) {
+    console.log("exception:" + ex);
+}
 p2.age = 44;
 p2.prenom = "axelle";
 p2.nom = "Aire";
--- tp-ts/src/personne.ts
+++ tp-ts/src/personne.ts
@@ -4,25 +4,43 @@ class Personne {
    numero : number;
    prenom : string;
    nom: string;
-   age : number;
+   private _age : number =0;
+
+   public get age(){
+       return this._age;
+   }
+
+   public set age(newAge : number){
+       if(newAge>=0)  this._age = newAge;
+       else throw "age negatif invalide";
+   }
 
    constructor(numero : number = 0, prenom : string ="?",  nom: string = "?", age : number=0){
        this.numero = numero; this.prenom = prenom; this.nom= nom; this.age=age;
    }
 
     incrementerAge() :void {
-        this.age++;
+        this._age++;
     } 
 }
 
-let p1 : Personne ;
-p1 = new Personne(1,"jean","Bon",33);
-p1.incrementerAge(); //nouvel age 34
-console.log(`prenom de p1=${p1.prenom}`)
-console.log(JSON.stringify(p1));
+try{
+    let p1 : Personne ;
+    p1 = new Personne(1,"jean","Bon",-33);
+    p1.incrementerAge(); //nouvel age 34
+    console.log(`prenom de p1=${p1.prenom}`)
+    console.log(JSON.stringify(p1));
+}catch(ex){
+    console.log("exception:" + ex);
+}
 
 let p2 : Personne ;
 p2 = new Personne();
+try{
+    p2.age=-44;
+}catch(ex){
+    console.log("exception:" + ex);
+}
 p2.age=44;
 p2.prenom="axelle"; p2.nom="Aire"
 p2.incrementerAge(); //nouvel age 45
