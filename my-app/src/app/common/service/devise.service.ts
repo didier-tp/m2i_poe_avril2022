e9a9dc3473805178075db439dae412302f1d3223
Didier Defrance <didier@d-defrance.fr>
2022-05-05 12:32:27 +0200

    avec appel ws rest

--- my-app/src/app/common/service/devise.service.ts
+++ my-app/src/app/common/service/devise.service.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, Observable, of } from 'rxjs';
+import { delay, map, Observable, of } from 'rxjs';
 import { Devise } from '../data/devise';
 
 @Injectable({
@@ -10,39 +11,33 @@ export class DeviseService {
   //= convention facultative de nom de fonction
   //pour dire que ça retourne un Observable
   rechercherDevises$() : Observable< Devise[]> {
-      //V1:
-      return of(this.tabDevises)
-             .pipe( delay(300)); //simuler un temps d'attente de 300ms pour obtenir la réponse
+      const url = "http://localhost:8282/devise-api/public/devise";
+      return this.http.get<Devise[]>(url);
   }
+
+  /*
+  serveur backend-tp-api
+  a URL
+  http://localhost:8282/devise-api/public/convert?source=EUR&target=USD&amount=200
+  renvoyant
+  {"source":"EUR","target":"USD","amount":200,"result":217.39130434782606}
+  */
 
   convertir$(codeDeviseSource:string ,
              codeDeviseCible:string , 
              montant: number ) : Observable<number>{
-    //V1 (version simulée sans serveur):
-    //return of(1.5); //simulation grossière .
-    const deviseSource = this.selectDeviseFromCode(codeDeviseSource);
-    const deviseCible = this.selectDeviseFromCode(codeDeviseCible);
-    if(deviseSource && deviseCible)
-        return of(montant * deviseCible.change / deviseSource.change)
-               .pipe( delay(300)); //simuler un temps d'attente de 300ms pour obtenir la réponse
-    else return of(0);
+    const url = `http://localhost:8282/devise-api/public/convert`
+         + `?source=${codeDeviseSource}&target=${codeDeviseCible}&amount=${montant}`;
+    return this.http.get<any>(url)
+               .pipe(
+                   map((respServer)=>respServer['result'])
+               );
   }
 
-  //V1 (version simulée sans serveur)
-  tabDevises : Devise[] = [
-    new Devise("EUR","Euro",1),
-    new Devise("USD","Dollar",1.1),
-    new Devise("GBP","Livre",0.9),
-    new Devise("JPY","Yen",120)
-  ];
+  
 
-  selectDeviseFromCode(codeDevise:string):Devise|null{
-    let dev:Devise|null=null;
-    for(const d of this.tabDevises){
-      if(d.code==codeDevise) dev = d;
-    }
-    return dev;
+  constructor(private http :HttpClient) { 
+    //injection de dépendance du service technique HttpClient
+    //dans notre service fonctionnel deviseService
   }
-
-  constructor() { }
 }
