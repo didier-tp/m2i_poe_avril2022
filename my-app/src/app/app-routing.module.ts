dfb26828694e12837406faf12ba1afd90fed6dad
Didier Defrance <didier@d-defrance.fr>
2022-05-09 14:50:26 +0200

    gardien

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { BasicComponent } from './basic/basic.component';
 import { CalculatriceComponent } from './basic/calculatrice/calculatrice.component';
 import { TvaComponent } from './basic/tva/tva.component';
+import { Gardien } from './common/gardien';
 import { ConvertisseurComponent } from './convertisseur/convertisseur.component';
 import { DemoComponent } from './demo/demo.component';
 import { DeviseV2Component } from './devise-v2/devise-v2.component';
 import { DeviseComponent } from './devise/devise.component';
 import { LoginComponent } from './login/login.component';
+import { NotAuthorizedComponent } from './not-authorized/not-authorized.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
@@ -21,8 +23,9 @@ const routes: Routes = [
    { path : "demo" , component  : DemoComponent } ,
    { path : "welcome" , component : WelcomeComponent },
    { path : "devise" , component : DeviseComponent },
-   { path : "deviseV2" , component : DeviseV2Component },
+   { path : "deviseV2" , component : DeviseV2Component, canActivate : [Gardien] },
    { path : "login" , component : LoginComponent },
+   { path : "not-authorized" , component : NotAuthorizedComponent },
    { path : "convertisseur" , component : ConvertisseurComponent },
    { path: '', redirectTo: '/welcome', pathMatch: 'full'}
 ];
--- my-app/src/app/common/gardien.ts
+++ my-app/src/app/common/gardien.ts
@@ -1,14 +1,25 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
+import { SessionService } from "./service/session.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class Gardien implements CanActivate{
+
+    constructor(private _sessionService: SessionService,
+                private _router : Router){
+        //injection de dépendance.
+    }
     
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        throw new Error("Method not implemented.");
+        if(this._sessionService.connecte){
+            return true;//navigation possible 
+        }else{
+            //return false; //navigation bloquée
+            return this._router.parseUrl('/not-authorized');
+        }
     }
     
 }
